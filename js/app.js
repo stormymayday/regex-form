21b3fb91ac45466f6aacee0f98619a42516a0919
stormymayday <stormymayday7@gmail.com>
2024-08-15 10:20:09 -0700

    Renamed 'telephone' input field to 'telephoneUS'. Created the 'validate' function that tests input field value against given regex and changes field class name to either 'valid' or 'invalid'

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 import getElement from "./utils/getElement.js";
 import { patterns } from "./utils/patterns.js";
 
-console.log(patterns);
-
 const inputs = document.querySelectorAll("input");
 
 inputs.forEach((input) => {
     input.addEventListener("keyup", (e) => {
-        console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value]);
     });
 });
+
+const validate = (field, regex) => {
+    if (regex.test(field.value)) {
+        field.className = "valid";
+    } else {
+        field.className = "invalid";
+    }
+};
