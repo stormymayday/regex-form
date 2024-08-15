48c591ada2e52fd91933f1cb7188f8810698f31c
stormymayday <stormymayday7@gmail.com>
2024-08-14 23:28:12 -0700

    Selecting all 'input' elements and attaching an on 'keyup' event listeners

--- validation.js
+++ validation.js
@@ -1,4 +1,14 @@
+import getElement from "./utils/getElement.js";
+
 const patterns = {
     telephoneUK: /^\d{11}$/,
     telephoneUS: /^[0-9]{10}$/,
 };
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", (e) => {
+        console.log(e.target.attributes.name.value);
+    });
+});
