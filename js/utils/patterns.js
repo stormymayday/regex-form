a52a934028d8c589961f83ce9aa0752156b5e327
stormymayday <stormymayday7@gmail.com>
2024-08-21 18:17:49 -0700

    Added regex for email

--- js/utils/patterns.js
+++ js/utils/patterns.js
@@ -3,4 +3,5 @@ export const patterns = {
     telephoneUS: /^[0-9]{10}$/,
     username: /^[a-z\d]{5,12}$/i,
     slug: /^[a-z0-9-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
 };
