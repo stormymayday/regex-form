46922ff5c93ae654dd0c34d988c9fb4d20f7933c
stormymayday <stormymayday7@gmail.com>
2024-08-21 18:02:52 -0700

    Added regex for the profile slug

--- js/utils/patterns.js
+++ js/utils/patterns.js
@@ -2,5 +2,5 @@ export const patterns = {
     telephoneUK: /^\d{11}$/,
     telephoneUS: /^[0-9]{10}$/,
     username: /^[a-z\d]{5,12}$/i,
-    password: /^[\w@-]{8,12}$/,
+    slug: /^[a-z0-9-]{8,20}$/,
 };
